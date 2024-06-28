206539b8e93339315f443bfc90bc291fac13c395
badalsah <badalsah8541@gmail.com>
2024-06-28 14:32:13 +0530

    env file change

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/orderRoute.js';
 
 const app = express()
-const port = 4000;
+const port = process.env.port || 4000;
 
 //middlewares
 app.use(express.json())
